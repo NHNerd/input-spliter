cd99b68f4334bb06aec04184c7156b6b8d4cf2db
NHNerd <promenader.off@gmail.com>
2023-10-03 00:00:27 +0400

    -fixed: arrow key cursor positioning(canceled)

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ import { inputSplitHandler } from './inputSplitHandler.js';
 const inputDOM = document.getElementById('input');
 
 //! * Workaround * (the algorithm breaks down when the input cursor is set to the middle)
+// cancel mouse cursor positioning
 inputDOM.style.pointerEvents = 'none';
-document.addEventListener('click', () => {
+document.addEventListener('click', (event) => {
   inputDOM.focus();
+});
+// cancel the arrow key cursor positioning
+inputDOM.addEventListener('keydown', (event) => {
+  if (event.keyCode === 37 || event.keyCode === 39) {
+    event.preventDefault();
+  }
 });
 
 // 1. part of text
